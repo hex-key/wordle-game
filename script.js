dcdd36cb9b369226d53cc9abb71b7f3fd12bf581
iso <irischen0505@gmail.com>
2022-01-23 02:34:34 -0800

    did stuff

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const word = "lmao";
+const btn = document.querySelector('#start-button');
+const textBox = document.querySelector('#text-box');
+
+//if enter key pressed while in text box 
+textBox.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        if (textBox.value.length === 5) {
+            alert("congrat");
+        } else {
+            alert("enter 5 characters idiot");
+        }
+    }
+});
+
+function startGame() {
+    
+}
